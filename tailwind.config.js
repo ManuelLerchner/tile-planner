dfb3e5780d4137f4aa03583228ebf90ab0ddd24e
ManuelLerchner <manuel.lerchner1111@gmail.com>
2023-04-16 23:18:13 +0200

    dont auto switch to darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   theme: {
     extend: {},
   },
   plugins: [],
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: ['light'],
+  }
 };
